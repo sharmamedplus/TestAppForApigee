07e6918000e1e6a93564f24f2dd091bea5bf0a13
Vijay Sharma <vijaysharma6760@gmail.com>
2021-02-09 15:45:40 +0530

    Initial

---
+++ src/test/java/com/sample/rest/SampleRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sample.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
